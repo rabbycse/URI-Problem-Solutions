a4b2bba69ed849828bb3015aa52d717357e47c91
@rabby <rabbycse7@gmail.com>
2019-03-20 18:39:47 +0600

    Extremely Basic

---
+++ 01/BEGINNER/URI-1001.cs
@@ -0,0 +1,19 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+       int A, B, X;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            X = A + B;
+          
+            Console.WriteLine("X = {0}", X);
+            Console.ReadLine();
+
+    }
+
+}
