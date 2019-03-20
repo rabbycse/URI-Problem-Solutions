28b5e6391c8edcaa26dc67cbe4157327d467bfe0
@rabby <rabbycse7@gmail.com>
2019-03-20 19:31:35 +0600

    Even Numbers

---
+++ 01/BEGINNER/URI-1059.cs
@@ -0,0 +1,14 @@
+using System; 
+
+class URI {
+
+        public static void Main()
+        {
+            int a;
+            for (a = 2; a <= 100; a = a + 2)
+                Console.WriteLine("{0}", a);
+
+            Console.ReadLine();
+        }
+
+}
