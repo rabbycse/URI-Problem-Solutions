6a13c2a06519f2811b4488d29f4423c9c249a749
@rabby <rabbycse7@gmail.com>
2019-03-20 19:31:03 +0600

    Month

---
+++ 01/BEGINNER/URI-1052.cs
@@ -0,0 +1,37 @@
+using System; 
+
+class URI {
+
+        public static void Main()
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            if (n == 1)
+                Console.WriteLine("January");
+            else if (n == 2)
+                Console.WriteLine("February");
+            else if (n == 3)
+                Console.WriteLine("March");
+            else if (n == 4)
+                Console.WriteLine("April");
+            else if (n == 5)
+                Console.WriteLine("May");
+            else if (n == 6)
+                Console.WriteLine("June");
+            else if (n == 7)
+                Console.WriteLine("July");
+            else if (n == 8)
+                Console.WriteLine("August");
+            else if (n == 9)
+                Console.WriteLine("September");
+            else if (n == 10)
+                Console.WriteLine("October");
+            else if (n == 11)
+                Console.WriteLine("November");
+            else if (n == 12)
+                Console.WriteLine("December");
+
+            Console.ReadLine();
+        }
+
+}
